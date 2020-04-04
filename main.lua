8d0f4265f8ab9ab962acb104fa9598ca74da48e8
tayadehritik <braceritchie@protonmail.com>
2020-04-04 22:51:14 +0530

    mobile touch introduced

--- main.lua
+++ main.lua
@@ -18,7 +18,13 @@ backgroundlayer5x = 0
 backgroundcolor = love.graphics.newImage("assets/layers/backgroundcolor.png")
 
 ship = love.graphics.newImage("assets/layers/ship.png")
+shipx = 896/4
+shipy = 414/2
 
+shipx2 = shipx + 180
+shipy2 = shipy + 74
+
+shipexpectedy = 414/2
 
 function love.load()
 
@@ -26,34 +32,63 @@ function love.load()
 
 end
 
+
+
+multiplier = 1
+
 function love.update(dt)
 
-    backgroundlayer3x = backgroundlayer3x - (1/3);
-    backgroundlayer2x = backgroundlayer2x - (1/4);
-    backgroundlayer1x = backgroundlayer1x - (1/5);
-    backgroundlayer0x = backgroundlayer0x - (1/6);
-
-    backgroundlayer5x = backgroundlayer5x - (1/2);
-
-    checkIfBackgroundImagesClipped()
+   
+    backgroundlayer0x = backgroundlayer0x - (multiplier/6)
+    backgroundlayer1x = backgroundlayer1x - (multiplier/5)
+    backgroundlayer2x = backgroundlayer2x - (multiplier/4)
+    backgroundlayer3x = backgroundlayer3x - (multiplier/3)
+    backgroundlayer5x = backgroundlayer5x - (multiplier/2)
     
+    checkIfBackgroundImagesClipped()
 
+    differenceShipY = shipy - shipexpectedy
+
+    if(shipexpectedy > shipy)
+    then
+        shipy = shipy + 1
+    else
+        if(shipy2 ~= shipexpectedy)
+        then
+            shipy = shipy - 1
+        end
+    end
 
 end
 
+x = 0
+globaly = 0
 
 function checkIfBackgroundImagesClipped()
 
-    if(backgroundlayer1x <= -896) then
-        backgroundlayer1x = 0
-    elseif(backgroundlayer2x <= -896) then
-        backgroundlayer2x = 0
-    elseif(backgroundlayer3x <= -896) then
-        backgroundlayer3x = 0
-    elseif(backgroundlayer0x <= -896) then
+    if(backgroundlayer0x <= -896) then
         backgroundlayer0x = 0
     end
 
+    if(backgroundlayer1x <= -896) then
+        backgroundlayer1x = 0
+    end
+
+    if(backgroundlayer2x <= -896) then
+        backgroundlayer2x = 0
+    end
+
+    if(backgroundlayer3x <= -896) then
+        backgroundlayer3x = 0
+    end
+
+
+    
+
+end
+function love.touchpressed( id, x, y, dx, dy, pressure )
+    -- test if the touch happened in the upper half of the screen
+    shipexpectedy = y
 end
 
 function love.draw()
@@ -62,10 +97,17 @@ function love.draw()
     love.graphics.draw(backgroundlayer1,backgroundlayer1x,0);
     love.graphics.draw(backgroundlayer2,backgroundlayer2x,0);
     love.graphics.draw(backgroundlayer3,backgroundlayer3x,0);
+    love.graphics.draw(backgroundlayer5,backgroundlayer5x,0);
+    --love.graphics.print(backgroundlayer1x,0,0)
+
     if(backgroundlayer5x >= -896-120)
     then
         love.graphics.draw(backgroundlayer5,backgroundlayer5x,0);
     end
 
-    love.graphics.draw(ship,896/3,412/2);
+    love.graphics.draw(ship,shipx,shipy);
+
+    local touches = love.touch.getTouches()
+    love.graphics.print(shipy,896/2,412/2);
+    
 end
